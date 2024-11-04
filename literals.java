29f3a150804623d8fe04b8a975ffdfb85d6dbe27
Shivam-dev478 <0987shivamsinghs@gmail.com>
2024-11-04 21:05:30 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,23 @@
+
+
+public class literals {
+    public static void main(String[] args) {
+         int integer=9;
+         float f_Number=9.8f;
+         char c='A';
+         long l=9555555555555l;
+         byte b=32;
+        boolean d=true;
+        double d1=499d;
+        String str="Shivam";
+         System.out.println(integer);
+         System.out.println(f_Number);
+         System.out.println(c);
+         System.out.println(l);
+         System.out.println(b);
+         System.out.println(d1);
+        System.out.println(d);
+        System.out.println(str);
+
+      }
+}
